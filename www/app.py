dc8950bb350dc4c946323ba2e164a7c360de94f9
Alfonso Villalobos <alfonsocvu@gmail.com>
2024-07-06 12:40:41 -0600

    ASD

---
+++ www/app.py
@@ -0,0 +1,19 @@
+import flask
+
+from www.webapp.index import webapp
+
+
+def create_app():
+    flask_app = flask.Flask(
+        __name__,
+        template_folder='webapp/static',
+        static_folder='webapp/static')
+
+    flask_app.register_blueprint(webapp)
+
+    return flask_app
+
+
+if __name__ == '__main__':
+    flask_app = create_app()
+    flask_app.run()
